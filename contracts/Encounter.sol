0005213410b923036f667af2185703b264592f8a
dermatologist <github@gulfdoctor.net>
2019-08-16 19:43:08 -0400

    Before new function

--- contracts/Encounter.sol
+++ contracts/Encounter.sol
@@ -26,12 +26,14 @@ contract Encounter
     mapping (uint64 => Vaccination) vaccinations;
 
 
-    function PatientAddRecord(uint64 vaccineCode, bytes32 hcn) public
+    function PatientAddRecord(uint64 vaccineCode, string memory _hcn) public
     {
         if (vaccineCode == 0)
         {
             revert();
         }
+
+        bytes32 hcn = stringToBytes32(_hcn);
 
         if (vaccinations[vaccineCode].patient.hcn_hash != keccak256(abi.encode(hcn))){
             revert();
@@ -39,29 +41,59 @@ contract Encounter
         vaccinations[vaccineCode].patient.addr = msg.sender;
     }
 
-    function ProviderAddRecord(uint64 vaccineCode, bytes32 hcn, bytes32 name) public
+    function ProviderAddRecord(uint64 vaccineCode, string memory _hcn, string memory _name) public
     {
         if (vaccineCode == 0)
         {
             revert();
         }
 
+        bytes32 hcn = stringToBytes32(_hcn);
+        bytes32 name = stringToBytes32(_name);
         vaccinations[vaccineCode].provider.addr = msg.sender;
         vaccinations[vaccineCode].patient.hcn_hash = keccak256(abi.encode(hcn));
         vaccinations[vaccineCode].vaccine.name = name;
     }
 
-    function CheckVaccination(uint64 vaccineCode, bytes32 hcn) public view returns (bytes32)
+    function CheckVaccination(uint64 vaccineCode, string memory _hcn) public view returns (string memory)
     {
         if (vaccineCode == 0)
         {
             revert();
         }
-
+        bytes32 hcn = stringToBytes32(_hcn);
         if (vaccinations[vaccineCode].patient.hcn_hash != keccak256(abi.encode(hcn))){
             revert();
         }
-        return vaccinations[vaccineCode].vaccine.name;
+        return bytes32ToString(vaccinations[vaccineCode].vaccine.name);
+    }
+
+    function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+
+    function bytes32ToString(bytes32 x) public pure returns (string memory) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (uint j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
     }
 
 }
